8043b7df2c9390c7f49f5311e4500ca73fa76ac7
storytotech <sochghazali@gmail.com>
2025-07-14 23:45:11 +0545

    second changes

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const http = require("http")
+import express from "express"
+import morgan from "morgan";
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-const server = http.createServer((req,res)=> {
-    console.log(req.url);
-    res.end("hello") }
-)
 
-server.listen(PORT,()=> console.log("this server is running of theport of " + PORT)
-) 
+app.set("view engine" , "ejs")
+app.use(morgan("dev"))
+app.use(morgan("combined"))
+
+app.get("/",(req,res)=>{res.send("helllo")
+})
+
+// app.get("/ejs",(req,res)=>{res.render('index')
+// })
+
+app.get("/ejs", (req,res,next)=>{morgan("combined")(req,res,next)},(req,res)=>{res.render('index')
+})
+
+app.get("/ee", (req,res,next)=>{morgan("combined");next()},(req,res)=>{res.render('index')
+})
+app.listen(PORT,()=>{console.log(`the server is running on the port of ${PORT}`)})
